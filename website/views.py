c51c29e7dd6d00aa9b3fd054c7492dd4b13208a6
ebrahimee.morteza <ebrahimee.morteza@surenacs.com>
2026-02-21 11:03:13 +0330

    added site and blog

---
+++ website/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse,JsonResponse
+
+def home_view(request):
+    return render(request,'website/index.html')
+
+
+def about_view(request):
+    return render(request,'website/about.html')
+
+def contact_view(request):
+    return render(request,'website/contact.html')
+
+def test_view(request):
+    return render(request,'website/test.html',{'name':'Morteza','lastname':'Ebrahimi'}) 
